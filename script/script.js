0c4cab6317acd8086e88e218a421c4335b8873ad
ElPopo18 <luisrbellizi@gmail.com>
2026-08-23 15:37:49 -0400

    Add attack function for character objects

--- script/script.js
+++ script/script.js
@@ -5,28 +5,42 @@ document.addEventListener('DOMContentLoaded', function () {
         this.health = health;
         this.weapon = weapon;
         this.secWeapon = secWeapon;
-    }
+    };
 
     function Enemy(name, health, weapon){
         if (!new.target) {
             throw Error("You must use the 'new' operator to call the constructor");
         };
         Character.call(this, name, health, weapon)
+    };
+
+    function getAttackDamage (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    Enemy.prototype.info = function(){
-        return `A ${this.name} with ${this.health}HP using a ${this.weapon}!`;
+    Character.prototype.attack = function(){
+        const minimumAttack = 57;
+        const maximumAttack = 74
+        const attackDamage = getAttackDamage(minimumAttack, maximumAttack);
+
+        console.log(attackDamage);
     };
 
     Character.prototype.info = function(){
         return `A ${this.name} with ${this.health}HP using a ${this.weapon} and a ${this.secWeapon}!`;
     };
 
-    Object.setPrototypeOf(Character.prototype, Enemy.prototype);
+    Enemy.prototype.info = function(){
+        return `A ${this.name} with ${this.health}HP using a ${this.weapon}!`;
+    };
 
-    let goblin = new Enemy("Goblin", 500, "knife");
-    let gladiator = new Character("Gladiator", 700, "Iron Sword", "Iron Shield")
-    console.log(goblin.info());
-    console.log(gladiator.info());
+    const goblin = new Enemy("Goblin", 500, "knife");
+    const gladiator = new Character("Gladiator", 700, "Iron Sword", "Iron Shield");
 
+    const attack = document.querySelector(".attack-button");
+    attack.addEventListener("click", (e) => {
+        gladiator.attack();
+        console.log(goblin.info());
+        console.log(gladiator.info());
+    })
 })
