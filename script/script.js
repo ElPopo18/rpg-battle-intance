95b0aee4b6ddaf7f6b3c7a6d085c515a381224ad
ElPopo18 <luisrbellizi@gmail.com>
2026-08-23 14:48:52 -0400

    Add enemy constructor with all enemy's stats, alongside goblin enemy

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+    function Enemy(name, health, weapon){
+        if (!new.target) {
+            throw Error("You must use the 'new' operator to call the constructor");
+        };
+        this.name = name;
+        this.health = health;
+        this.weapon = weapon;
+    }
+
+    Enemy.prototype.info = function(){
+        return `A ${this.name} with ${this.health}HP using a ${this.weapon}!`;
+    }
+
+    let goblin = new Enemy("Goblin", 500, "knife");
+    console.log(goblin.info());
+
 })
